5998e58b655ef3e63b72989a131b12675654b701
Noelsky-code <hansory@naver.com>
2020-07-12 17:15:21 +0900

    sadasd

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,18 @@
 import React,{Component} from 'react';
 import './Todo.css';
-import {Input,Button,FormGroup} from 'reactstrap';
+import {Input,Button,FormGroup,ButtonGroup} from 'reactstrap';
 
 class Todo extends Component {
  render(){
-   console.log(this.props)
     return(
-        <div className="Todo">{this.props.name}</div>
+  
+        <div className="Todo">
+          <div className="Text">{this.props.name}</div>
+         <ButtonGroup className="Buttons">
+          <Button align = "right" color = "success" size ="sm">o</Button>
+          <Button color ="danger" size ="sm">x</Button>
+          </ButtonGroup>
+        </div>
     );
   }
 }
