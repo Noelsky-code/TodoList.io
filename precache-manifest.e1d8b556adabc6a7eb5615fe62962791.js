aba2d3a8580fd8b5aa7709b5f027feb837336318
Noelsky <66724340+Noelsky-code@users.noreply.github.com>
2021-06-09 21:33:13 +0900

    test

---
+++ precache-manifest.e1d8b556adabc6a7eb5615fe62962791.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2efef6778a463bb49a6cb4fb34a4eb3",
+    "url": "/TodoList.io/index.html"
+  },
+  {
+    "revision": "cf2d5caf245b66c1b712",
+    "url": "/TodoList.io/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bb8f20a47ed06e1dc082",
+    "url": "/TodoList.io/static/css/main.1125f3b9.chunk.css"
+  },
+  {
+    "revision": "cf2d5caf245b66c1b712",
+    "url": "/TodoList.io/static/js/2.9cb2bf26.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/TodoList.io/static/js/2.9cb2bf26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb8f20a47ed06e1dc082",
+    "url": "/TodoList.io/static/js/main.8dad76f9.chunk.js"
+  },
+  {
+    "revision": "58188b0cb34104cbd296",
+    "url": "/TodoList.io/static/js/runtime-main.e5fe4b49.js"
+  }
+]);
