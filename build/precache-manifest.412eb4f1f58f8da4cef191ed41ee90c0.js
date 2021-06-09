d5d1a5c73b40f2ee894349cbad0c8e25b5d85da1
Noelsky <66724340+Noelsky-code@users.noreply.github.com>
2021-06-09 21:12:47 +0900

    test

---
+++ build/precache-manifest.412eb4f1f58f8da4cef191ed41ee90c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e231cbaa7572c044f79b4ec2c4d579fd",
+    "url": "/noelsky-code/todoList.io/index.html"
+  },
+  {
+    "revision": "cf2d5caf245b66c1b712",
+    "url": "/noelsky-code/todoList.io/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fa97881d6f6217f52c82",
+    "url": "/noelsky-code/todoList.io/static/css/main.1125f3b9.chunk.css"
+  },
+  {
+    "revision": "cf2d5caf245b66c1b712",
+    "url": "/noelsky-code/todoList.io/static/js/2.9cb2bf26.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/noelsky-code/todoList.io/static/js/2.9cb2bf26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa97881d6f6217f52c82",
+    "url": "/noelsky-code/todoList.io/static/js/main.e6b958ed.chunk.js"
+  },
+  {
+    "revision": "d2f83a1cff3838f46248",
+    "url": "/noelsky-code/todoList.io/static/js/runtime-main.6ecda074.js"
+  }
+]);
